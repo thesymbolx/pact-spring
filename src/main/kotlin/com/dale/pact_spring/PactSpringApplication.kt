27dc46484ad7b0da9b3e52b842b115c43f3ce8f2
Dale Evans <dale.evans1984@gmail.com>
2026-04-09 20:19:23 +0100

    first commit

---
+++ src/main/kotlin/com/dale/pact_spring/PactSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.dale.pact_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PactSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<PactSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/dale/pact_spring/PactSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dale.pact_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PactSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
